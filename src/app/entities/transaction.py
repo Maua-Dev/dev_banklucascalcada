37e4d58dfac20edfa5f212634ee5ff7eb5b55a74
Lucas <lucasmgmcalcada@gmail.com>
2023-08-14 23:10:48 -0300

    Transaction object

---
+++ src/app/entities/transaction.py
@@ -0,0 +1,37 @@
+from enum import Enum
+from time import time
+
+class TransactionType(Enum):
+    DEPOSIT = "deposit"
+    WITHDRAW = "withdraw"
+
+class Transaction:
+    def __init__(self, transactionType:TransactionType, value:float, currentBalance:float) -> None:
+        assert value > 0, "Value must be higher than zero"
+        self.__type = transactionType
+        self.__value = value
+        self.__currentBalance = currentBalance
+        self.__timestamp = time()
+
+    def toDict(self) -> dict:
+        return {
+            "type": self.__type.value,
+            "value": self.__value,
+            "current_balance": self.__currentBalance,
+            "timestamp": self.__timestamp
+        }
+
+    @property 
+    def transactionType(self) -> TransactionType:
+        return self.__type
+
+    @property
+    def value(self) -> float:
+        return self.__value
+
+    @property
+    def currentBalance(self) -> float:
+        return self.__currentBalance
+    
+if __name__ == "__main__":
+    a = Transaction(TransactionType.DEPOSIT, 10.0, 10.0)
--- tests/app/entities/test_item.py
+++ tests/app/entities/test_item.py
@@ -1,55 +1,14 @@
 import pytest
-from src.app.entities.item import Item
-from src.app.enums.item_type_enum import ItemTypeEnum
-from src.app.errors.entity_errors import ParamNotValidated
+from src.app.entities.transaction import Transaction, TransactionType
 
 
-class Test_Item:
-    def test_item(self):
-        item = Item("test", 1.0, ItemTypeEnum.FOOD, admin_permission=True)
-        assert item.name == "test"
-        assert item.price == 1.0
-        assert item.item_type == ItemTypeEnum.FOOD
-        
-    def test_item_dict(self):
-        item = Item("test", 1.0, ItemTypeEnum.FOOD, admin_permission=True)
-        assert item.to_dict() == {'admin_permission': True, 'item_type': 'FOOD', 'name': 'test', 'price': 1.0}
-    
-    def test_item_name_is_none(self):
-        with pytest.raises(ParamNotValidated):
-            Item(price=1.0, item_type=ItemTypeEnum.FOOD, admin_permission=True)
-            
-    def test_item_name_is_not_string(self):
-        with pytest.raises(ParamNotValidated):
-            Item(name=1.0, price=1.0, item_type=ItemTypeEnum.FOOD, admin_permission=True)
-            
-    def test_item_name_is_too_short(self):
-        with pytest.raises(ParamNotValidated):
-            Item(name="te", price=1.0, item_type=ItemTypeEnum.FOOD, admin_permission=True)
-            
-    def test_item_price_is_none(self):
-        with pytest.raises(ParamNotValidated):
-            Item(name="test", item_type=ItemTypeEnum.FOOD, admin_permission=True)
-            
-    def test_item_price_is_not_float(self):
-        with pytest.raises(ParamNotValidated):
-            Item(name="test", price="1.0", item_type=ItemTypeEnum.FOOD, admin_permission=True)
-    def test_item_price_is_negative(self):
-        with pytest.raises(ParamNotValidated):
-            Item(name="test", price=-1.0, item_type=ItemTypeEnum.FOOD, admin_permission=True)
-            
-    def test_item_type_is_none(self):
-        with pytest.raises(ParamNotValidated):
-            Item(name="test", price=1.0, admin_permission=True)
-            
-    def test_item_type_is_not_enum(self):
-        with pytest.raises(ParamNotValidated):
-            Item(name="test", price=1.0, item_type="FOOD", admin_permission=True)
-            
-    def test_item_admin_permission_is_none(self):
-        with pytest.raises(ParamNotValidated):
-            Item(name="test", price=1.0, item_type=ItemTypeEnum.FOOD)
-        
-    def test_item_admin_permission_is_not_bool(self):
-        with pytest.raises(ParamNotValidated):
-            Item(name="test", price=1.0, item_type=ItemTypeEnum.FOOD, admin_permission="True")
+def test_transaction():
+    transactionType = TransactionType.DEPOSIT
+    value = 10.0
+    currentBalance = 10.0
+    t = Transaction(transactionType, value, currentBalance)
+
+    assert t.transactionType == transactionType
+    assert t.value == value
+    assert t.currentBalance == currentBalance
+    assert type(t.toDict()) == dict
